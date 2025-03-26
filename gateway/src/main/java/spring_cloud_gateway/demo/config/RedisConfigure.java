51405571e8e6370c214eda8efecc6263ea7717a0
Luis-For <luisfor90@gmail.com>
2025-03-26 03:40:32 -0500

    Add beans for Redis configuration

---
+++ gateway/src/main/java/spring_cloud_gateway/demo/config/RedisConfigure.java
@@ -0,0 +1,32 @@
+package spring_cloud_gateway.demo.config;
+
+import java.time.Duration;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+@EnableCaching
+public class RedisConfigure {
+    @Bean   //conexion a redis
+    public RedisConnectionFactory redisConnectionFactory(){
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory){
+        //configuración del tiempo de expiración del cache
+        RedisCacheConfiguration redisCacheConfiguration=RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(10));
+        return RedisCacheManager.builder(redisConnectionFactory).cacheDefaults(redisCacheConfiguration).build();
+    }
+
+    //@Bean
+    public RedisTemplate<String, >
+}
